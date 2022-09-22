bdaa20da4de79ff33171e2272d91403fb17b56b2
mc_fdc <Masato.04.11.2007@gmail.com>
2022-09-22 14:41:14 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,13 @@
 from janome.tokenizer import Tokenizer
 
 
-text = "TokenCountFilter を使うと，入力文字列中の単語出現頻度を数えることができます。".replace(" ", "")
+text = "なんか僕よりも彼氏みたい。".replace(" ", "")
 model = {}
 last = ""
 
 t = Tokenizer()
 for token in t.tokenize(text):
     message = str(token).split()[0]
-    print(message in model)
     if message in model:
         model[last].append(message)
     else:
