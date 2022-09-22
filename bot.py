e07956b197bd7094ce2fff697457a6282c80df4a
mc_fdc <Masato.04.11.2007@gmail.com>
2022-09-22 14:32:24 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,8 @@ last = ""
 
 t = Tokenizer()
 for token in t.tokenize("マイクラしたい。"):
-    print(type(token))
+    if token is None:
+        continue
     message = token.split()[0]
     if message in model:
         model[message].append(last)
