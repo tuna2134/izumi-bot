05368ff494cb63f96ef6841c0016f266bc99b07e
mc_fdc <Masato.04.11.2007@gmail.com>
2022-09-22 15:26:23 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,21 @@
 from janome.tokenizer import Tokenizer
 
 
-text = "なんか僕よりも彼氏みたい。".replace(" ", "")
+with open("data/message.txt") as f:
+    texts = [m.replace(" ", "") for m in f.readlines()]
 model = {}
 last = ""
 
 t = Tokenizer()
-for token in t.tokenize(text):
-    message = str(token).split()[0]
-    if last == "":
+for text in texts:
+    for token in t.tokenize(text):
+        message = str(token).split()[0]
+        if last == "":
+            last = message
+            continue
+        if message in model:
+            model[last].append(message)
+        else:
+            model[last] = [message]
         last = message
-        continue
-    if message in model:
-        model[last].append(message)
-    else:
-        model[last] = [message]
-    last = message
 print(model)
