14b4d06ade5ebaf8f24c214bb5cb652722d9443f
mc_fdc <Masato.04.11.2007@gmail.com>
2022-09-22 14:29:07 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,15 @@
 from janome.tokenizer import Tokenizer
 
 
+model = {}
+last = ""
+
 t = Tokenizer()
 for token in t.tokenize("マイクラしたい。"):
-    print(token)
+    message = token.split()[0]
+    if message in model:
+        model[message].append(last)
+    else:
+        model[message] = [last]
+    last = message
+print(model)
