745ab64c45dab7e5b98b41c748354800bae21290
mc_fdc <Masato.04.11.2007@gmail.com>
2022-09-22 14:38:18 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
 from janome.tokenizer import Tokenizer
 
 
+text = "TokenCountFilter を使うと，入力文字列中の単語出現頻度を数えることができます。".replace(" ", "")
 model = {}
 last = ""
 
 t = Tokenizer()
-for token in t.tokenize("TokenCountFilter を使うと，入力文字列中の単語出現頻度を数えることができます。"):
+for token in t.tokenize(text):
     print(token)
     print(model)
     message = str(token).split()[0]
