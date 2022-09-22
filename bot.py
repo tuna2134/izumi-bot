f805127a26f8874eca24bd2b06cda4f8c83d97fc
mc_fdc <Masato.04.11.2007@gmail.com>
2022-09-22 14:36:04 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ last = ""
 
 t = Tokenizer()
 for token in t.tokenize("TokenCountFilter を使うと，入力文字列中の単語出現頻度を数えることができます。"):
+    print(token)
     message = str(token).split()[0]
     if message in model:
         model[last].append(message)
