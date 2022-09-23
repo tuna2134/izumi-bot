b1c22771be3eccdfa8a83bed5260509412eb0ab3
mc_fdc <Masato.04.11.2007@gmail.com>
2022-09-23 21:37:53 +0900

    Update createm.py

--- createm.py
+++ createm.py
@@ -6,11 +6,14 @@ with open("model.json") as f:
     model = json.load(f)
 
 last = random.choice(model["__BEGIN__"])
+begin = "__BEGIN__"
 message = [last]
 
 while True:
     last = random.choice(model[last])
+    model[last].remove(begin)
     message.append(last)
+    begin = last
     if last in ["。", "！", "？"]:
         break
 
