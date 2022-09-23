78bb7c1d3f964bb1c96c98209c2eb4078960ce84
mc_fdc <Masato.04.11.2007@gmail.com>
2022-09-23 21:43:03 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,6 @@ last = "__BEGIN__"
 t = Tokenizer("data/userdic.csv", udic_enc="utf8")
 for text in texts:
     for token in t.tokenize(text):
-        print(token)
         message = str(token).split()[0]
         if last in ["。", "！", "？"]:
             last = message
@@ -21,6 +20,7 @@ for text in texts:
         else:
             model[last] = [message]
         last = message
+    last = "__BEGIN__"
 print(model)
 
 with open("model.json", "w") as f:
